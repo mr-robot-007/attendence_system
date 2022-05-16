cffd6d6038c78175d66d6f9e134b0aa31b4e20f8
Anuj Gusain <anujgusain108@gmail.com>
2022-05-16 10:31:50 +0530

    first commit

---
+++ attendence_sys/forms.py
@@ -0,0 +1,27 @@
+from tkinter import Widget
+from django import forms
+from django.forms import ModelForm
+
+from .models import *
+
+class CreateStudentForm(ModelForm):
+   
+    class Meta:
+        model = Student
+        fields = '__all__'
+        
+    def __init__(self, *args, **kwargs):
+        super(CreateStudentForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+            
+    
+class FacultyForm(ModelForm):
+    class Meta:
+        model = Faculty
+        fields = '__all__'
+        exclude = ['user']
+    def __init__(self, *args, **kwargs):
+        super(FacultyForm, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'    
